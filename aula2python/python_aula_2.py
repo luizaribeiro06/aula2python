7713600252415ce384b45871bcb708ce954292c1
Luiza Ribeiro <84139409+luizaribeiro06@users.noreply.github.com>
2024-09-12 21:00:18 -0300

    Add files via upload

---
+++ aula2python/python_aula_2.py
@@ -0,0 +1,35 @@
+# a = 10
+# b = 2
+# print ("a antigo: ", a, "/","b antigo: ", b)
+# print()
+#
+# aux = a
+# a = b
+# b = aux
+# print ("a novo: ", a, "/", "b novo: ", b)
+#
+# n1= 2
+# n2 = 3
+# n3 = 4
+#
+# soma = (n1 + n2 + n3)
+# print(soma)
+
+# dia = 1
+# hora = 3
+# min = 46
+# seg = 40
+# seg_total = (min * 60) + (hora * 3600) + (dia *86400)+ (seg)
+# print("Total de segundos:" , seg_total)
+
+#1min = 60seg
+#1h = 3600
+#1dia= 86400
+
+# seg_total = 100000
+# seg = seg_total%60
+#
+# hora = (seg_total) // 3600
+# min = (seg_total%3600) // 60
+#
+# print( hora, "horas", min, "minutos",seg, "segundos")
