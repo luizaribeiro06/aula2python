1540b5a6bccb0b1bafa83de47dec911af51440d1
Luiza Ribeiro <84139409+luizaribeiro06@users.noreply.github.com>
2024-09-12 22:50:46 -0300

    Add files via upload

---
+++ python_aula_2.py
@@ -0,0 +1,70 @@
+﻿# a = 10
+# b = 2
+# print ("a antigo: ", a, "/","b antigo: ", b)
+# print()
+#
+# aux = a
+# a = b
+# b = aux
+# print ("a novo: ", a, "/", "b novo: ", b)
+#
+# n1= 2
+# n2 = 3
+# n3 = 4
+#
+# soma = (n1 + n2 + n3)
+# print(soma)
+
+# dia = 1
+# hora = 3
+# min = 46
+# seg = 40
+# seg_total = (min * 60) + (hora * 3600) + (dia *86400)+ (seg)
+# print("Total de segundos:" , seg_total)
+
+#1min = 60seg
+#1h = 3600
+#1dia= 86400
+
+# seg_total = 100000
+# seg = seg_total%60
+#
+# hora = (seg_total) // 3600
+# min = (seg_total%3600) // 60
+#
+# print( hora, "horas", min, "minutos",seg, "segundos")
+
+# a = True #valor booleano com 1letra maiúscula
+# print(3+4 == 7)
+# a = 3+4 == 7
+# print (a)
+#
+# c = 5.0
+# f = 5
+# print(c!=f)
+
+#media, retornar se foi aprovado (true) ou não (false)
+# n1= 5
+# n2 = 6
+# n3 = 8
+# media = (n1 + n2 + n3)/3
+# print(media >=6)
+
+#paga imposto quem ganha mais do q 1200
+# salario = 1300
+# valor_imposto = 1200
+# print(salario > valor_imposto)
+
+#operadores lógicos (not, and, or)
+
+#com base em duas condições, determine se vai poder fazer empréstimo ou não
+# idade = 18
+# salario = 1200
+# carteirada = idade<=18 or salario<=1300 ORRRR 1
+# print(idade>=18 and salario>=1300 or carteirada==1) isso pq se pelo menos um for verdadeiro, já fica verdadeiro
+
+
+#retornar TRUE or FALSE verificando se um número é par
+num= 6
+par = num%2 == 0
+print(par)
